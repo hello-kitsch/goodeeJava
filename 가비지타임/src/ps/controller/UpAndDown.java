e7df8cc03ac4281f21be9156515ef2ed33a55900
hello-kitsch <107244836+hello-kitsch@users.noreply.github.com>
2024-02-29 07:53:45 +0900

    UpAndDown.java 업데이트

--- 가비지타임/src/ps/controller/UpAndDown.java
+++ 가비지타임/src/ps/controller/UpAndDown.java
@@ -2,7 +2,22 @@ package ps.controller;
 
 public class UpAndDown {
     //up&down게임 만들기
-	//랜덤으로 1~50까지 수 한개를 저장하고
-	//사용자가 입력하는 값과 비교해서 크면 up, 작으면 다운 출력
-	//맞출때까지 반복 맞추면 정답, 00회만에 맞췄습니다 출력
+   	//랜덤으로 1~50까지 수 한개를 저장하고
+	   //사용자가 입력하는 값과 비교해서 크면 up, 작으면 다운 출력
+    //맞출때까지 반복 맞추면 정답, 00회만에 맞췄습니다 출력
+    public void updown() {
+        int rnd = (int)(Math.random()*50) + 1;
+        Scanner sc = new Scanner(System.in);
+        int count = 1;
+        while(true) {
+            System.out.print("정수 입력: ");
+            int num = sc.nextInt();
+            if(num == rnd) {
+                System.out.println("정답, " + count + "회만에 맞췄습니다.");
+                break;
+            }
+            if(num > rnd) System.out.println("down");
+            if(num < rnd) System.out.println("up");
+        }
+    }
 }
